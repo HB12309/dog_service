7f479c46dc1c71e38111ae4e72f8b6a62d419eba
qiuhengbin <qiuhengbin@codemao.cn>
2019-03-12 21:13:36 +0800

    feat: 完成 hello world

--- dog-service/src/main/java/ebing/top/dog/service/DogServiceApplication.java
+++ dog-service/src/main/java/ebing/top/dog/service/DogServiceApplication.java
@@ -1,11 +1,13 @@
 package ebing.top.dog.service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@MapperScan("ebing.top.dog.service.mapper")
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableTransactionManagement
--- dog-service/src/main/java/ebing/top/dog/service/client/impl/BackdoorClientImpl.java
+++ dog-service/src/main/java/ebing/top/dog/service/client/impl/BackdoorClientImpl.java
@@ -1,9 +1,17 @@
 package ebing.top.dog.service.client.impl;
 
 import ebing.top.dog.client.BackdoorClient;
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "后门模块", description = "backdoor 模块相关接口")
+@Validated
+@RequestMapping("/backdoor")
+@RestController
 @Service
 public class BackdoorClientImpl implements BackdoorClient {
 
