2674793574a28998e7aac7c64a4a32e54ae76b95
qiuhengbin <qiuhengbin@codemao.cn>
2019-03-15 17:37:49 +0800

    add : stream

--- dog-client/src/main/java/ebing/top/dog/service/client/LanguageClient.java
+++ dog-client/src/main/java/ebing/top/dog/service/client/LanguageClient.java
@@ -3,6 +3,7 @@ package ebing.top.dog.service.client;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author dog
@@ -16,6 +17,8 @@ public interface LanguageClient {
 	 * @return String
 	 */
 	@GetMapping("/thread")
-	String thread();
+	String thread(
+		@RequestParam(value = "type", required = false)  String type
+	);
 
 }
--- dog-service/src/main/java/ebing/top/dog/service/client/impl/LanguageClientImpl.java
+++ dog-service/src/main/java/ebing/top/dog/service/client/impl/LanguageClientImpl.java
@@ -1,15 +1,13 @@
 package ebing.top.dog.service.client.impl;
 
-import ebing.top.dog.service.Utils.BinaryTree;
 import ebing.top.dog.service.client.LanguageClient;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
 
 @Api(tags = "language模块", description = "language 模块相关接口")
@@ -21,10 +19,26 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class LanguageClientImpl implements LanguageClient {
 
+	/**
+	 * 哦，在调用第二个方法的时候，我的流已经被使用了，这样是不可以的。
+	 *
+	 * 也就是说：一个 Stream 只可以使用一次
+	 */
+	private void streamAPI() {
+		Stream<String> stream = Stream.of("", "1", "11", "111", "1111", "11111", "Hollis");
+		stream.filter( i -> i.length() < 4).forEach(System.out::println);
+	}
+
 	@Override
 	@GetMapping("/thread")
-	public String thread() {
-		Integer a = ThreadLocalRandom.current().nextInt();
-		return Integer.toString(ThreadLocalRandom.current().nextInt(0) + 100000);
+	public String thread(
+		@RequestParam(value = "type", required = false) @ApiParam("意见反馈") String type
+	) {
+		if ("stream".equals(type)) {
+			Integer a = ThreadLocalRandom.current().nextInt();
+			streamAPI();
+			return Integer.toString(ThreadLocalRandom.current().nextInt(10) + 100000);
+		}
+		return "success";
 	}
 }
