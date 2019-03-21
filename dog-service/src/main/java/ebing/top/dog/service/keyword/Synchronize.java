4e8a7ac4a64f1e00be1d77c792f87edc38517d70
qiuhengbin <qiuhengbin@codemao.cn>
2019-03-21 20:32:18 +0800

    增加 AtomicInteger CAS (compare and swap)，原子操作，并发编程，连续调用

--- dog-service/src/main/java/ebing/top/dog/service/keyword/Synchronize.java
+++ dog-service/src/main/java/ebing/top/dog/service/keyword/Synchronize.java
@@ -7,6 +7,9 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 通过volatile修饰的变量可以保证线程之间的可见性，但并不能保证这3个指令的原子执行，在多线程并发执行下，无法做到线程安全，
  * 在add方法加上synchronized修饰解决。但是性能上差了点
  *
+ * 可以使用 synchronized 或者使用 Atomic 开头的（原子操作）
+ * AtomicInteger 使用了 CAS，而 CAS 底层使用JNI调用C代码实现的
+ *
  */
 public class Synchronize {
 	private volatile int n;
@@ -14,4 +17,13 @@ public class Synchronize {
 	public synchronized void add() {
 		n++;
 	}
+
+
+//	public AtomicInteger i;
+//
+//	public void add() {
+//		i.getAndIncrement();
+//	}
 }
+
+
